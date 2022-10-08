ad03fd0673b611b64eca5875af57e6d05311f999
Justin <justin@aperturebot.science>
2022-10-08 00:01:01 -0400

    TODOs

--- app/src/main/java/edu/farmingdale/bcs421/bcs421_f22_w6_fragmentsdemo/MainActivity2.kt
+++ app/src/main/java/edu/farmingdale/bcs421/bcs421_f22_w6_fragmentsdemo/MainActivity2.kt
@@ -1,14 +1,21 @@
 package edu.farmingdale.bcs421.bcs421_f22_w6_fragmentsdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity2 : AppCompatActivity() {
     lateinit var mBtn1:Button
     lateinit var mBtn2:Button
     lateinit var mTV:TextView
+    lateinit var edit:EditText
+    lateinit var seek:SeekBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +24,26 @@ class MainActivity2 : AppCompatActivity() {
         mBtn1 = findViewById(R.id.btn1);
         mBtn2 = findViewById(R.id.btn2);
         mTV = findViewById(R.id.tv1)
+        seek = findViewById(R.id.seekBar)
+        edit = findViewById(R.id.editText01)
         var frgmnt01 = Fragment01()
         var frgmnt02 = Fragment02()
+
+        seek.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+
+            }
+
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                // TODO Auto-generated method stub
+                mTV.setTextSize(progress.toFloat())
+                edit.setText(progress.toString())
+            }
+        })
 
 
         mBtn1.setOnClickListener{
